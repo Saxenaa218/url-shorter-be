b9b3bc98ee1a6db56cf08e23a400b7de9e5fa1c7
Abhishek <saxenaa218@gmail.com>
2024-12-27 12:27:40 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req: any, res: any) => {
+  return res.send("Express Typescript on Vercel");
+});
+
+app.get("/ping", (req: any, res: any) => {
+  return res.send("pong 🏓");
+});
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
